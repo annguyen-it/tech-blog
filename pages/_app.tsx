85709179746eb8d650563086b813259fb267a031
annguyen <annguyen.it.utc@gmail.com>
2022-11-04 09:25:42 +0700

    chore: improve SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -35,6 +35,12 @@ function MyApp({ Component, pageProps }: AppProps<PageProps>) {
   return (
     <>
       <Head>
+        <html lang="en" />
+        <meta charSet="UTF-8" />
+        <meta name="description" content="Tech blog is a community of 947,500 amazing developers" />
+        <meta name="keywords" content="tech, blog, code" />
+        <meta name="author" content="John Doe" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
 
